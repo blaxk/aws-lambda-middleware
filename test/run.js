fca41d299135a23ae6c7d71166db1f6effb2ae85
이진오 <jino@jino.local>
2020-06-02 14:21:46 +0900

    Lambda Payload 2.0 support

--- test/run.js
+++ test/run.js
@@ -86,43 +86,100 @@ const handler3 = new Middleware().add(async (event, context, prevData) => {
 
 /** ========== Run Handler ========== */
 
-handler1({
-	"resource": "/users/create",
-	"source": "serverless-plugin-warmup",
-	"path": "/users/create",
-	"httpMethod": "GET",
-	"headers": {
-		"User-Agent": "PostmanRuntime/7.6.0",
-		"X-Forwarded-For": "175.1.1.1",
-		"Content-Type": "application/json",
+const palyload1 = {
+	version: '1.0',
+	resource: '/my/path',
+	path: '/my/path',
+	httpMethod: 'GET',
+	headers: {
+		'Header1': 'value1',
+		'Header2': 'value2'
 	},
-	"queryStringParameters": {
-		"username": "test"
+	multiValueHeaders: {
+		'Header1': ['value1'],
+		'Header2': ['value1', 'value2']
 	},
-	"multiValueQueryStringParameters": null,
-	"pathParameters": {
-		"username": "test"
-	},
-	"stageVariables": null,
-	"requestContext": {
-		"authorizer": {
-			"username": "testId",
-			"userId": 12
+	queryStringParameters: { parameter1: 'value1', parameter2: 'value' },
+	multiValueQueryStringParameters: { parameter1: ['value1', 'value2'], paramter2: ['value'] },
+	requestContext: {
+		accountId: '123456789012',
+		apiId: 'id',
+		authorizer: { claims: null, scopes: null },
+		domainName: 'id.execute-api.us-east-1.amazonaws.com',
+		domainPrefix: 'id',
+		extendedRequestId: 'request-id',
+		httpMethod: 'GET',
+		identity: {
+			accessKey: null,
+			accountId: null,
+			caller: null,
+			cognitoAuthenticationProvider: null,
+			cognitoAuthenticationType: null,
+			cognitoIdentityId: null,
+			cognitoIdentityPoolId: null,
+			principalOrgId: null,
+			sourceIp: 'IP',
+			user: null,
+			userAgent: 'user-agent',
+			userArn: null
 		},
-		"path": "/users/create",
-		"accountId": "000000000000",
-		"resourceId": "skkkk00",
-		"stage": "dev",
-		"domainPrefix": "testPrefix",
-		"domainName": "testPrefix.testDomainName",
-		"resourcePath": "/users/create",
-		"httpMethod": "GET",
-		"extendedRequestId": "VwTpXAACoE0FV7w=",
-		"apiId": "ujhfkdkd0988"
+		path: '/my/path',
+		protocol: 'HTTP/1.1',
+		requestId: 'id=',
+		requestTime: '04/Mar/2020:19:15:17 +0000',
+		requestTimeEpoch: 1583349317135,
+		resourceId: null,
+		resourcePath: '/my/path',
+		stage: '$default'
 	},
-	"body": "{\"username\":\"1\"}",
-	"isBase64Encoded": false
-}, {
+	pathParameters: null,
+	stageVariables: null,
+	body: 'Hello from Lambda!',
+	isBase64Encoded: true
+}
+
+const palyload2 = {
+	version: '2.0',
+	routeKey: '$default',
+	rawPath: '/my/path',
+	rawQueryString: 'parameter1=value1&parameter1=value2&parameter2=value',
+	cookies: ['cookie1', 'cookie2'],
+	headers: {
+		'Header1': 'value1',
+		'Header2': 'value2'
+	},
+	queryStringParameters: { parameter1: 'value1,value2', parameter2: 'value' },
+	requestContext: {
+		accountId: '123456789012',
+		apiId: 'api-id',
+		authorizer: {
+			jwt: {
+				claims: { 'claim1': 'value1', 'claim2': 'value2' },
+				scopes: ['scope1', 'scope2']
+			}
+		},
+		domainName: 'id.execute-api.us-east-1.amazonaws.com',
+		domainPrefix: 'id',
+		http: {
+			method: 'POST',
+			path: '/my/path',
+			protocol: 'HTTP/1.1',
+			sourceIp: 'IP',
+			userAgent: 'agent'
+		},
+		requestId: 'id',
+		routeKey: '$default',
+		stage: '$default',
+		time: '12/Mar/2020:19:03:58 +0000',
+		timeEpoch: 1583348638390
+	},
+	body: 'Hello from Lambda',
+	pathParameters: { 'parameter1': 'value1' },
+	isBase64Encoded: false,
+	stageVariables: { 'stageVariable1': 'value1', 'stageVariable2': 'value2' }
+}
+
+handler1(palyload2, {
 
 }, (err, callbackData) => {
 		//callback
