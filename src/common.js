90cf4c6e6b5b6c65912105177c710d830e528f3c
이진오 <jino@jino.local>
2020-08-03 15:27:38 +0900

    사용하지 않는 convertDataType 삭제

--- src/common.js
+++ src/common.js
@@ -26,28 +26,6 @@ const Common = {
 		return /^\[[^\]]+Error\]$/.test(Object.prototype.toString.call(value))
 	},
 
-	convertDataType: (str) => {
-		if (str && typeof str === 'string') {
-			if (/^true$/.test(str)) {
-				str = Boolean(str)
-			} else if (/^false$/.test(str)) {
-				str = Boolean()
-			} else if (/^-*[0-9\.]+$/.test(str)) {
-				str = str.replace(/\s/g, '')
-
-				if (!/^0[0-9]+/.test(str) && !/^-0[0-9]+/.test(str) && !(str.length === 1 && str === '-')) {
-					str = Number(str)
-				}
-			} else if (/^null$/.test(str)) {
-				str = null
-			} else if (/^undefined$/.test(str)) {
-				str = undefined
-			}
-		}
-
-		return str
-	},
-
 	/**
 	 * get header
 	 * @param {Object}	event
